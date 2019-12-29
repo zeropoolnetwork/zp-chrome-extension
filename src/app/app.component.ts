60f7d1e2a8688d56371806d1d4ccef599837c1f1
artem <artall64@gmail.com>
2019-12-29 21:46:05 +0200

    Configured custom webpack build with live reload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'zp-chrome-extension';
+  constructor() {
+    console.log('Hi');
+    debugger
+  }
 }
---
+++ src/background.ts
@@ -0,0 +1,11 @@
+// chrome.runtime.onInstalled.addListener(() => {
+//   console.log('Background has started');
+//
+//   chrome.webNavigation.onCompleted.addListener(() => {
+//     chrome.tabs.query({ active: true, currentWindow: true }, ([{ id }]) => {
+//       chrome.pageAction.show(id);
+//     });
+//   }, { url: [{ urlMatches: 'google.com' }] });
+// });
+
+console.log('Background has started 1!');
