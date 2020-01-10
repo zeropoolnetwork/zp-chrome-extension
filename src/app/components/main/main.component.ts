248b1ae8698f9002a63fa8940ab4c268ad89a877
Nick Kozlov <nk@buttonwallet.com>
2020-01-10 17:37:50 +0300

    ui

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {from} from 'rxjs';
+import {map, take} from 'rxjs/operators';
+import {toShortAddress} from '../header/header.component';
+import {StorageService} from '../../services/storage.service';
 
 @Component({
   selector: 'app-main',
@@ -6,11 +10,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
+  currentAddress: string;
+  currentName: string;
 
-  constructor() {
+  constructor(private storage: StorageService) {
+    from(this.storage.getAccountPublicData()).pipe(
+      map((data) => {
+        this.currentName = data.accounts[0].public_details.name;
+        this.currentAddress = toShortAddress(data.accounts[0].public_details.addresses[0].value);
+      }),
+      take(1)
+    ).subscribe();
   }
 
   ngOnInit() {
   }
 
 }
+
