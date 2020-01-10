4e0709173dc34a41cb4c055e924e437500b9d7e0
Nick Kozlov <nk@buttonwallet.com>
2020-01-10 22:41:31 +0300

    added hover to UI

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -3,6 +3,7 @@ import {from} from 'rxjs';
 import {map, take} from 'rxjs/operators';
 import {toShortAddress} from '../header/header.component';
 import {StorageService} from '../../services/storage.service';
+import {ClipboardService} from '../../services/clipboard.service';
 
 @Component({
   selector: 'app-main',
@@ -11,13 +12,16 @@ import {StorageService} from '../../services/storage.service';
 })
 export class MainComponent implements OnInit {
   currentAddress: string;
+  currentAddressShort: string;
   currentName: string;
+  copyMessage = 'Copy address';
 
-  constructor(private storage: StorageService) {
+  constructor(private storage: StorageService, private clipboard: ClipboardService) {
     from(this.storage.getAccountPublicData()).pipe(
       map((data) => {
         this.currentName = data.accounts[0].public_details.name;
-        this.currentAddress = toShortAddress(data.accounts[0].public_details.addresses[0].value);
+        this.currentAddressShort = toShortAddress(data.accounts[0].public_details.addresses[0].value);
+        this.currentAddress = data.accounts[0].public_details.addresses[0].value;
       }),
       take(1)
     ).subscribe();
@@ -26,5 +30,9 @@ export class MainComponent implements OnInit {
   ngOnInit() {
   }
 
+  copyMnemonicToClipboard() {
+    this.clipboard.copyToClipboard(this.currentAddress);
+    this.copyMessage = 'Copied';
+  }
 }
 
