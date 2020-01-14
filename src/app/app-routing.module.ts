8e2b8baab3c24ad4f92c41d885e72dbc4e109427
Nick Kozlov <nk@buttonwallet.com>
2020-01-14 19:52:04 +0300

    routing done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,29 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { WelcomeComponent } from './components/welcome/welcome.component';
-import { ImportAccountComponent } from './components/import-account/import-account.component';
-import { CreateAccountComponent } from './components/create-account/create-account.component';
-import { MainComponent } from './components/main/main.component';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {WelcomeComponent} from './components/welcome/welcome.component';
+import {ImportAccountComponent} from './components/import-account/import-account.component';
+import {CreateAccountComponent} from './components/create-account/create-account.component';
+import {MainComponent} from './components/main/main.component';
 import {PasswordComponent} from './components/password/password.component';
 import {RepeatPasswordComponent} from './components/repeat-password/repeat-password.component';
 import {MainMenuComponent} from './components/main-menu/main-menu.component';
 import {ShowMnemonicComponent} from './components/show-mnemonic/show-mnemonic.component';
 import {DeleteAccountApproveComponent} from './components/delete-account-approve/delete-account-approve.component';
+import {UnlockComponent} from './components/unlock/unlock.component';
+import {AuthGuardService as AuthGuard} from './services/auth-guard.service';
 
 const routes: Routes = [
   {path: '', component: WelcomeComponent},
+  {path: 'welcome/:operation', component: WelcomeComponent},
   {path: 'import', component: ImportAccountComponent},
   {path: 'create', component: CreateAccountComponent},
-  {path: 'password/:operation', component: PasswordComponent},
-  {path: 'repeat', component: RepeatPasswordComponent},
-  {path: 'main', component: MainComponent},
-  {path: 'menu', component: MainMenuComponent},
-  {path: 'mnemonic', component: ShowMnemonicComponent},
-  {path: 'delete', component: DeleteAccountApproveComponent},
+  {path: 'password/:operation', component: PasswordComponent, canActivate: [AuthGuard]},
+  {path: 'repeat', component: RepeatPasswordComponent, canActivate: [AuthGuard]},
+  {path: 'unlock', component: UnlockComponent},
+  {path: 'main', component: MainComponent, canActivate: [AuthGuard]},
+  {path: 'menu', component: MainMenuComponent, canActivate: [AuthGuard]},
+  {path: 'mnemonic', component: ShowMnemonicComponent, canActivate: [AuthGuard]},
+  {path: 'delete', component: DeleteAccountApproveComponent, canActivate: [AuthGuard]},
 ];
 
 @NgModule({
--- src/app/components/welcome/welcome.component.ts
+++ src/app/components/welcome/welcome.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {StorageService} from '../../services/storage.service';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-welcome',
@@ -7,7 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WelcomeComponent implements OnInit {
 
-  constructor() { }
+  constructor( private storage: StorageService,
+               private activatedRoute: ActivatedRoute,
+               private router: Router ) {
+    try {
+      const operation = this.activatedRoute.snapshot.params.operation;
+      this.routerNavigation(operation);
+    } catch (e) {
+      this.routerNavigation();
+    }
+
+  }
+
+  async routerNavigation( operation?: string ) {
+    if (operation !== 'new') {
+      const result = await this.checkExists();
+      if (result) {
+        await this.router.navigate(['/unlock']);
+      }
+    }
+  }
+
+  async checkExists(): Promise<boolean> {
+    let data;
+    try {
+      data = (await this.storage.getAccountPublicData()).accounts.length;
+    } catch (e) {
+      data = 0;
+    }
+
+    return data !== 0;
+  }
 
   ngOnInit() {
   }
